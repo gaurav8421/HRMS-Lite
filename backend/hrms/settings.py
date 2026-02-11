78f18166f3fad95ca3e08fd73e55f92efc9c455a
gaurav-pulse <gaurav@pulsepro.ai>
2026-02-12 03:02:08 +0530

    main_branch

--- backend/hrms/settings.py
+++ backend/hrms/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production-
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS configuration
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1','https://hrms-lite-mu-nine.vercel.app').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','https://hrms-lite-mu-nine.vercel.app').split(',')
 # Add Render hostname if deployed
 RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
